22a053537a9578e525d67aea3ebcd10acf9c6fe0
Murthy <vmurthy@uspto.gov>
2018-07-02 13:08:37 -0400

    clean up

--- Other/p2.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('filingVolumes.csv', newline='') as myFile:
-    reader = csv.reader(myFile)
-    for row in reader:
-        if row==[] or row==['filing_date']:
-            continue
